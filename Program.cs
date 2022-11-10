f52028e63b7f0d157e79da89d78e73c954966dc9
Tomás Basso Fernández <tomas.basso@hotmail.com>
2022-11-10 20:21:35 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ACT6_
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            int[] arreglonumeros = { 10, 20, 30, 4, 9, 8, 4, 6, 10, 20 };
+            int valor;
+
+            Console.WriteLine("Ingrese un numero: ");
+            valor = Convert.ToInt32(Console.ReadLine());
+
+            BuscarEnArreglo(arreglonumeros, valor);
+
+        }
+         static void BuscarEnArreglo(int[] arreglonumeros, int valor)
+        {
+            bool encontrado = false;
+            for (int i = 0; i < arreglonumeros.Length; i++)
+            {
+                if (arreglonumeros[i] == valor)
+                {
+                    encontrado = true;
+                }
+            }
+            if (encontrado == true)
+            {
+                Console.WriteLine("El valor fue hallado");
+            }
+            else
+            {
+                Console.WriteLine("El valor no fue hallado");
+            }
+        }
+    }
+}
+
